19393f6576110712eabe47c3f145ebc081274d45
Flikas <Flikas@Flikas-PC>
2017-09-21 23:50:47 +0800

    update

--- java-server-esb-sif/src/main/java/hello/Receiver.java
+++ java-server-esb-sif/src/main/java/hello/Receiver.java
@@ -1,22 +1,34 @@
 package hello;
 
 import org.springframework.stereotype.Component;
+import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.*;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.stream.StreamResult;
+
 import java.io.*;
 
 @Component
 public class Receiver {
 
-	public String receiveMessage(String message) throws ParserConfigurationException, SAXException, IOException {
-//		System.out.println("Received <" + message + ">");
-//		System.out.println("Response is <"+message+">");
+	public String receiveMessage(String message) throws ParserConfigurationException, SAXException, IOException,
+			TransformerConfigurationException, TransformerException, TransformerFactoryConfigurationError {
+		// System.out.println("Received <" + message + ">");
+		// System.out.println("Response is <"+message+">");
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		DocumentBuilder db = dbf.newDocumentBuilder();
-		StringReader sr = new StringReader(message);
-		new java.
-		db.parse(new java.io.StringBufferInputStream(message));
-		return message;
+		Document doc = db.parse(new java.io.ByteArrayInputStream(message.getBytes()));
+//		doc.getElementById("str").setNodeValue("a");
+		doc.appendChild(doc.createTextNode("abc"));
+		javax.xml.transform.dom.DOMSource ds = new javax.xml.transform.dom.DOMSource(doc);
+		java.io.StringWriter sw = new StringWriter();
+		StreamResult rst = new StreamResult(sw);
+		javax.xml.transform.TransformerFactory.newInstance().newTransformer(ds).transform(ds, rst);
+
+		return sw.toString();
 	}
 }
