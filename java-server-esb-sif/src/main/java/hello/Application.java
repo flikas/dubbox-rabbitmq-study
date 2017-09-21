0563bb91e2fe28c01b4d145691ec5527121acbc5
flikas <flikas@hotmail.com>
2017-09-21 22:34:32 +0800

    all

--- java-server-esb-sif/src/main/java/hello/Application.java
+++ java-server-esb-sif/src/main/java/hello/Application.java
@@ -1,42 +1,40 @@
 package hello;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
-	final static String queueName = "rpc_queue";
+	private static String queueName = "rpc_queue";
+	private static String host = "192.168.56.10";
 
 	@Bean
 	Queue queue() {
 		return new Queue(queueName, false);
 	}
 
-//	@Bean
-//	DirectExchange exchange() {
-//		return new DirectExchange("amq.direct");
-////		return new TopicExchange("amq.direct");
-//	}
-//
-//	@Bean
-//	Binding binding(Queue queue, TopicExchange exchange) {
-//		return BindingBuilder.bind(queue).to(exchange).with(queueName);
-//	}
-	
+	// @Bean
+	// DirectExchange exchange() {
+	// return new DirectExchange("amq.direct");
+	//// return new TopicExchange("amq.direct");
+	// }
+	//
+	// @Bean
+	// Binding binding(Queue queue, TopicExchange exchange) {
+	// return BindingBuilder.bind(queue).to(exchange).with(queueName);
+	// }
+
 	@Bean
-	ConnectionFactory connectionFactory(){
-		CachingConnectionFactory cf = new CachingConnectionFactory("192.168.56.10");
+	ConnectionFactory connectionFactory() {
+		CachingConnectionFactory cf = new CachingConnectionFactory(host);
 		cf.setUsername("flikas");
 		cf.setPassword("pengfei");
 		cf.createConnection();
@@ -59,8 +57,11 @@ public class Application {
 	}
 
 	public static void main(String[] args) throws InterruptedException {
-		System.out.println("Receving...");
+		if (args.length >= 1)
+			host = args[0];
+		if (args.length >= 2)
+			queueName = args[1];
+		System.out.println("Receving from " + host + " queue " + queueName);
 		SpringApplication.run(Application.class, args);
 	}
-
 }
--- java-server-esb-sif/src/main/java/hello/Receiver.java
+++ java-server-esb-sif/src/main/java/hello/Receiver.java
@@ -1,13 +1,22 @@
 package hello;
 
 import org.springframework.stereotype.Component;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.*;
+import java.io.*;
 
 @Component
 public class Receiver {
 
-	public String receiveMessage(String message) {
-		System.out.println("Received <" + message + ">");
-		System.out.println("Response is <"+message+">");
+	public String receiveMessage(String message) throws ParserConfigurationException, SAXException, IOException {
+//		System.out.println("Received <" + message + ">");
+//		System.out.println("Response is <"+message+">");
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		StringReader sr = new StringReader(message);
+		new java.
+		db.parse(new java.io.StringBufferInputStream(message));
 		return message;
 	}
 }
